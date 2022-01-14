bf7f74e23f268ef5c071b10a1a00ece729848f37
killer2k1 <95661986+killer2k1@users.noreply.github.com>
2022-01-14 11:49:17 -0500

    Delete PracticeMode.acs.cs

--- PracticeMode.acs.cs
+++
@@ -1,4 +0,0 @@
-editActionMap("playMap.sae");
-bindCommand(keyboard0, make, "insert", TO, "remoteEval(2048, StorePosition);");
-bindCommand(keyboard0, make, "delete", TO, "remoteEval(2048, RestorePosition);");
-bindCommand(keyboard0, make, "home", TO, "remoteEval(2048, RestoreHealth);");
