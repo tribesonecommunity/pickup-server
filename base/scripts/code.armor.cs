951a4df08b3be9004c494cf78f3c330d2e364c72
tribesonecommunity <dwayneyuen@gmail.com>
2022-08-21 09:27:11 -0700

    refactor: Add other armors back to code.armor.cs

--- base/scripts/code.armor.cs
+++ base/scripts/code.armor.cs
@@ -54,6 +54,114 @@ $ItemMax[larmor, DeployableAmmoPack] = 0;
 $MaxWeapons[larmor] = 3;
 
 //----------------------------------------------------------------------------
+// Medium Armor
+//----------------------------------------------------------------------------
+$DamageScale[marmor, $LandingDamageType] = 1.0;
+$DamageScale[marmor, $ImpactDamageType] = 1.0;
+$DamageScale[marmor, $CrushDamageType] = 1.0;
+$DamageScale[marmor, $BulletDamageType] = 1.0;
+$DamageScale[marmor, $PlasmaDamageType] = 0.6;
+$DamageScale[marmor, $EnergyDamageType] = 1.0;
+$DamageScale[marmor, $ExplosionDamageType] = 1.0;
+$DamageScale[marmor, $MissileDamageType] = 1.0;
+$DamageScale[marmor, $ShrapnelDamageType] = 1.0;
+$DamageScale[marmor, $DebrisDamageType] = 1.0;
+$DamageScale[marmor, $LaserDamageType] = 1.0;
+$DamageScale[marmor, $MortarDamageType] = 1.0;
+$DamageScale[marmor, $BlasterDamageType] = 1.0;
+$DamageScale[marmor, $ElectricityDamageType] = 1.0;
+$DamageScale[marmor, $MineDamageType] = 1.0;
+
+$ItemMax[marmor, Blaster] = 1;
+$ItemMax[marmor, Chaingun] = 1;
+$ItemMax[marmor, Disclauncher] = 1;
+$ItemMax[marmor, GrenadeLauncher] = 1;
+$ItemMax[marmor, Mortar] = 0;
+$ItemMax[marmor, PlasmaGun] = 1;
+$ItemMax[marmor, LaserRifle] = 0;
+$ItemMax[marmor, EnergyRifle] = 1;
+$ItemMax[marmor, TargetingLaser] = 1;
+$ItemMax[marmor, MineAmmo] = 3;
+$ItemMax[marmor, Grenade] = 6;
+$ItemMax[marmor, Beacon] = 3;
+
+$ItemMax[marmor, BulletAmmo] = 150;
+$ItemMax[marmor, PlasmaAmmo] = 40;
+$ItemMax[marmor, DiscAmmo] = 15;
+$ItemMax[marmor, GrenadeAmmo] = 10;
+$ItemMax[marmor, MortarAmmo] = 10;
+
+$ItemMax[marmor, EnergyPack] = 1;
+$ItemMax[marmor, RepairPack] = 1;
+$ItemMax[marmor, ShieldPack] = 1;
+$ItemMax[marmor, SensorJammerPack] = 1;
+$ItemMax[marmor, MotionSensorPack] = 1;
+$ItemMax[marmor, PulseSensorPack] = 1;
+$ItemMax[marmor, DeployableSensorJammerPack] = 1;
+$ItemMax[marmor, CameraPack] = 1;
+$ItemMax[marmor, TurretPack] = 1;
+$ItemMax[marmor, AmmoPack] = 1;
+$ItemMax[marmor, RepairKit] = 1;
+$ItemMax[marmor, DeployableInvPack] = 1;
+$ItemMax[marmor, DeployableAmmoPack] = 1;
+
+$MaxWeapons[marmor] = 4;
+
+//----------------------------------------------------------------------------
+// Heavy Armor
+//----------------------------------------------------------------------------
+$DamageScale[harmor, $LandingDamageType] = 1.0;
+$DamageScale[harmor, $ImpactDamageType] = 1.0;
+$DamageScale[harmor, $CrushDamageType] = 1.0;
+$DamageScale[harmor, $BulletDamageType] = 0.6;
+$DamageScale[harmor, $PlasmaDamageType] = 0.4;
+$DamageScale[harmor, $EnergyDamageType] = 0.7;
+$DamageScale[harmor, $ExplosionDamageType] = 0.6;
+$DamageScale[harmor, $MissileDamageType] = 0.6;
+$DamageScale[harmor, $DebrisDamageType] = 0.8;
+$DamageScale[harmor, $ShrapnelDamageType] = 0.8;
+$DamageScale[harmor, $LaserDamageType] = 0.6;
+$DamageScale[harmor, $MortarDamageType] = 0.7;
+$DamageScale[harmor, $BlasterDamageType] = 0.7;
+$DamageScale[harmor, $ElectricityDamageType] = 1.0;
+$DamageScale[harmor, $MineDamageType] = 0.8;
+
+$ItemMax[harmor, Blaster] = 1;
+$ItemMax[harmor, Chaingun] = 1;
+$ItemMax[harmor, Disclauncher] = 1;
+$ItemMax[harmor, GrenadeLauncher] = 1;
+$ItemMax[harmor, Mortar] = 1;
+$ItemMax[harmor, PlasmaGun] = 1;
+$ItemMax[harmor, LaserRifle] = 0;
+$ItemMax[harmor, EnergyRifle] = 1;
+$ItemMax[harmor, TargetingLaser] = 1;
+$ItemMax[harmor, MineAmmo] = 3;
+$ItemMax[harmor, Grenade] = 8;
+$ItemMax[harmor, Beacon] = 3;
+
+$ItemMax[harmor, BulletAmmo] = 200;
+$ItemMax[harmor, PlasmaAmmo] = 50;
+$ItemMax[harmor, DiscAmmo] = 15;
+$ItemMax[harmor, GrenadeAmmo] = 15;
+$ItemMax[harmor, MortarAmmo] = 10;
+
+$ItemMax[harmor, EnergyPack] = 1;
+$ItemMax[harmor, RepairPack] = 1;
+$ItemMax[harmor, ShieldPack] = 1;
+$ItemMax[harmor, SensorJammerPack] = 1;
+$ItemMax[harmor, MotionSensorPack] = 1;
+$ItemMax[harmor, PulseSensorPack] = 1;
+$ItemMax[harmor, DeployableSensorJammerPack] = 1;
+$ItemMax[harmor, CameraPack] = 1;
+$ItemMax[harmor, TurretPack] = 1;
+$ItemMax[harmor, AmmoPack] = 1;
+$ItemMax[harmor, RepairKit] = 1;
+$ItemMax[harmor, DeployableInvPack] = 1;
+$ItemMax[harmor, DeployableAmmoPack] = 1;
+
+$MaxWeapons[harmor] = 5;
+
+//----------------------------------------------------------------------------
 // light Female Armor
 //----------------------------------------------------------------------------
 $DamageScale[lfemale, $LandingDamageType] = 1.0;
@@ -106,3 +214,57 @@ $ItemMax[lfemale, DeployableInvPack] = 0;
 $ItemMax[lfemale, DeployableAmmoPack] = 0;
 
 $MaxWeapons[lfemale] = 3;
+
+//----------------------------------------------------------------------------
+// Medium Female Armor
+//----------------------------------------------------------------------------
+$DamageScale[mfemale, $LandingDamageType] = 1.0;
+$DamageScale[mfemale, $ImpactDamageType] = 1.0;
+$DamageScale[mfemale, $CrushDamageType] = 1.0;
+$DamageScale[mfemale, $BulletDamageType] = 1.0;
+$DamageScale[mfemale, $EnergyDamageType] = 1.0;
+$DamageScale[mfemale, $PlasmaDamageType] = 0.6;
+$DamageScale[mfemale, $ExplosionDamageType] = 1.0;
+$DamageScale[mfemale, $MissileDamageType] = 1.0;
+$DamageScale[mfemale, $ShrapnelDamageType] = 1.0;
+$DamageScale[mfemale, $DebrisDamageType] = 1.0;
+$DamageScale[mfemale, $LaserDamageType] = 1.0;
+$DamageScale[mfemale, $MortarDamageType] = 1.0;
+$DamageScale[mfemale, $BlasterDamageType] = 1.0;
+$DamageScale[mfemale, $ElectricityDamageType] = 1.0;
+$DamageScale[mfemale, $MineDamageType] = 1.0;
+
+$ItemMax[mfemale, Blaster] = 1;
+$ItemMax[mfemale, Chaingun] = 1;
+$ItemMax[mfemale, Disclauncher] = 1;
+$ItemMax[mfemale, GrenadeLauncher] = 1;
+$ItemMax[mfemale, Mortar] = 0;
+$ItemMax[mfemale, PlasmaGun] = 1;
+$ItemMax[mfemale, LaserRifle] = 0;
+$ItemMax[mfemale, EnergyRifle] = 1;
+$ItemMax[mfemale, TargetingLaser] = 1;
+$ItemMax[mfemale, MineAmmo] = 3;
+$ItemMax[mfemale, Grenade] = 6;
+$ItemMax[mfemale, Beacon] = 3;
+
+$ItemMax[mfemale, BulletAmmo] = 150;
+$ItemMax[mfemale, PlasmaAmmo] = 40;
+$ItemMax[mfemale, DiscAmmo] = 15;
+$ItemMax[mfemale, GrenadeAmmo] = 10;
+$ItemMax[mfemale, MortarAmmo] = 10;
+
+$ItemMax[mfemale, EnergyPack] = 1;
+$ItemMax[mfemale, RepairPack] = 1;
+$ItemMax[mfemale, ShieldPack] = 1;
+$ItemMax[mfemale, SensorJammerPack] = 1;
+$ItemMax[mfemale, MotionSensorPack] = 1;
+$ItemMax[mfemale, PulseSensorPack] = 1;
+$ItemMax[mfemale, DeployableSensorJammerPack] = 1;
+$ItemMax[mfemale, CameraPack] = 1;
+$ItemMax[mfemale, TurretPack] = 1;
+$ItemMax[mfemale, AmmoPack] = 1;
+$ItemMax[mfemale, RepairKit] = 1;
+$ItemMax[mfemale, DeployableInvPack] = 1;
+$ItemMax[mfemale, DeployableAmmoPack] = 1;
+
+$MaxWeapons[mfemale] = 4;
