cdd2f88a9faadd4154ca7a4b1e57a0240aa0ed16
tribesonecommunity <dwayneyuen@gmail.com>
2022-08-21 09:41:31 -0700

    refactor: revert code pack cs

--- base/scripts/code.pack.cs
+++ base/scripts/code.pack.cs
@@ -1,3 +1,17 @@
+//----------------------------------------------------------------------------
+
+function RepairGun::onMount(%player,%imageSlot)
+{
+	Player::trigger(%player,$BackpackSlot,true);
+}
+
+function RepairGun::onUnmount(%player,%imageSlot)
+{
+	Player::trigger(%player,$BackpackSlot,false);
+}
+
+//----------------------------------------------------------------------------
+
 function Backpack::onUse(%player,%item)
 {
 	if (Player::getMountedItem(%player,$BackpackSlot) != %item) {
@@ -7,6 +21,124 @@ function Backpack::onUse(%player,%item)
 		Player::trigger(%player,$BackpackSlot);
 	}
 }
+
+
+//----------------------------------------------------------------------------
+
+function DeployableInvPack::onUse(%player,%item)
+{
+	if (Player::getMountedItem(%player,$BackpackSlot) != %item) {
+		Player::mountItem(%player,%item,$BackpackSlot);
+	}
+	else {
+		Player::deployItem(%player,%item);
+	}
+}
+
+function DeployableInvPack::onDeploy(%player,%item,%pos)
+{
+	if (DeployableInvPack::deployShape(%player,%item)) {
+		Player::decItemCount(%player,%item);
+	}
+}	
+
+function DeployableInvPack::deployShape(%player,%item)
+{
+	%client = Player::getClient(%player);
+	if($TeamItemCount[GameBase::getTeam(%player) @ %item] < $TeamItemMax[%item]) {
+		if (GameBase::getLOSInfo(%player,3)) {
+			%obj = getObjectType($los::object);
+			if (%obj == "SimTerrain" || %obj == "InteriorShape") {
+				if (Vector::dot($los::normal,"0 0 1") > 0.7) {
+					if(checkDeployArea(%client,$los::position)) {
+						%inv = newObject("ammounit_remote","StaticShape","DeployableInvStation",true);
+ 	 		         addToSet("MissionCleanup", %inv);
+						%rot = GameBase::getRotation(%player); 
+						GameBase::setTeam(%inv,GameBase::getTeam(%player));
+						GameBase::setPosition(%inv,$los::position);
+						GameBase::setRotation(%inv,%rot);
+						Gamebase::setMapName(%inv,%name);
+						Client::sendMessage(%client,0,"Inventory Station deployed");
+						playSound(SoundPickupBackpack,$los::position);
+						$TeamItemCount[GameBase::getTeam(%inv) @ "DeployableInvPack"]++;
+						echo("MSG: ",%client," deployed an Inventory Station");
+						return true;
+					}
+				}
+				else {
+					Client::sendMessage(%client,0,"Can only deploy on flat surfaces");
+				}
+			}
+			else {
+				Client::sendMessage(%client,0,"Can only deploy on terrain or buildings");
+			}
+		}
+		else {
+			Client::sendMessage(%client,0,"Deploy position out of range");
+		}
+	}
+	else																						  
+	 	Client::sendMessage(%client,0,"Deployable Item limit reached for " @ %item.description @ "s");
+	return false;
+}
+
+//----------------------------------------------------------------------------
+
+function DeployableAmmoPack::onUse(%player,%item)
+{
+	if (Player::getMountedItem(%player,$BackpackSlot) != %item) {
+		Player::mountItem(%player,%item,$BackpackSlot);
+	}
+	else {
+		Player::deployItem(%player,%item);
+	}
+}
+
+function DeployableAmmoPack::onDeploy(%player,%item,%pos)
+{
+	if (DeployableAmmoPack::deployShape(%player,%item)) {
+		Player::decItemCount(%player,%item);
+	}
+}	
+
+function DeployableAmmoPack::deployShape(%player,%item)
+{
+	%client = Player::getClient(%player);
+	if($TeamItemCount[GameBase::getTeam(%player) @ %item] < $TeamItemMax[%item]) {
+		if (GameBase::getLOSInfo(%player,3)) {
+			%obj = getObjectType($los::object);
+			if (%obj == "SimTerrain" || %obj == "InteriorShape") {
+				if (Vector::dot($los::normal,"0 0 1") > 0.7) {
+					if(checkDeployArea(%client,$los::position)) {
+						%inv = newObject("ammounit_remote","StaticShape","DeployableAmmoStation",true);
+	         	   addToSet("MissionCleanup", %inv);
+						%rot = GameBase::getRotation(%player); 
+						GameBase::setTeam(%inv,GameBase::getTeam(%player));
+						GameBase::setPosition(%inv,$los::position);
+						GameBase::setRotation(%inv,%rot);
+						Gamebase::setMapName(%inv,%name);
+						Client::sendMessage(%client,0,"Ammo Station deployed");
+						playSound(SoundPickupBackpack,$los::position);
+						$TeamItemCount[GameBase::getTeam(%inv) @ "DeployableAmmoPack"]++;
+						echo("MSG: ",%client," deployed an Ammo Station");
+						return true;
+					}
+				}
+				else 
+					Client::sendMessage(%client,0,"Can only deploy on flat surfaces");
+			}
+			else 
+				Client::sendMessage(%client,0,"Can only deploy on terrain or buildings");
+		}
+		else 
+			Client::sendMessage(%client,0,"Deploy position out of range");
+	}
+	else																						  
+	 	Client::sendMessage(%client,0,"Deployable Item limit reached for " @ %item.description @ "s");
+	return false;
+}
+
+//----------------------------------------------------------------------------
 
 function EnergyPack::onUse(%player,%item)
 {
@@ -25,6 +157,285 @@ function EnergyPack::onUnmount(%player,%item)
 	if (Player::getMountedItem(%player,$WeaponSlot) == LaserRifle) 
 		Player::unmountItem(%player,$WeaponSlot);
 }
+
+//----------------------------------------------------------------------------
+
+function RepairPack::onUnmount(%player,%item)
+{
+	if (Player::getMountedItem(%player,$WeaponSlot) == RepairGun) {
+		Player::unmountItem(%player,$WeaponSlot);
+	}
+}
+
+function RepairPack::onUse(%player,%item)
+{
+	if (Player::getMountedItem(%player,$BackpackSlot) != %item) {
+		Player::mountItem(%player,%item,$BackpackSlot);
+	}
+	else {
+		Player::mountItem(%player,RepairGun,$WeaponSlot);
+	}
+}
+
+function RepairPack::onDrop(%player,%item)
+{
+	if($matchStarted) {
+		%mounted = Player::getMountedItem(%player,$WeaponSlot);
+		if (%mounted == RepairGun) {
+			Player::unmountItem(%player,$WeaponSlot);
+		}
+		else {
+			// Remount the existing weapon to make sure the RepairGun
+			// is not on the delayed mount "stack".
+			Player::mountItem(%player,%mounted,$WeaponSlot);
+		}
+		Item::onDrop(%player,%item);
+	}
+}	
+
+//----------------------------------------------------------------------------
+
+function ShieldPackImage::onActivate(%player,%imageSlot)
+{
+	Client::sendMessage(Player::getClient(%player),0,"Shield On");
+	%player.shieldStrength = 0.012;
+}
+
+function ShieldPackImage::onDeactivate(%player,%imageSlot)
+{
+	Client::sendMessage(Player::getClient(%player),0,"Shield Off");
+	Player::trigger(%player,$BackpackSlot,false);
+	%player.shieldStrength = 0;
+}
+
+//----------------------------------------------------------------------------
+
+function SensorJammerPackImage::onActivate(%player,%imageSlot)
+{
+	Client::sendMessage(Player::getClient(%player),0,"Sensor Jammer On");
+	%rate = Player::getSensorSupression(%player) + 20;
+	Player::setSensorSupression(%player,%rate);
+}
+
+function SensorJammerPackImage::onDeactivate(%player,%imageSlot)
+{
+	Client::sendMessage(Player::getClient(%player),0,"Sensor Jammer Off");
+	%rate = Player::getSensorSupression(%player) - 20;
+	Player::setSensorSupression(%player,%rate);
+	Player::trigger(%player,$BackpackSlot,false);
+}
+
+//----------------------------------------------------------------------------
+
+function MotionSensorPack::onUse(%player,%item)
+{
+	if (Player::getMountedItem(%player,$BackpackSlot) != %item) {
+		Player::mountItem(%player,%item,$BackpackSlot);
+	}
+	else {
+		Player::deployItem(%player,%item);
+	}
+}
+
+function MotionSensorPack::onDeploy(%player,%item,%pos)
+{
+	if (MotionSensorPack::deployShape(%player,%item)) {
+		Player::decItemCount(%player,%item);
+		$TeamItemCount[GameBase::getTeam(%player) @ "MotionSensorPack"]++;
+	}
+}
+
+//	if (Item::deployShape(%player,"Motion Sensor",MotionSensor,%item)) {
+function MotionSensorPack::deployShape(%player,%item)
+{
+ 	%client = Player::getClient(%player);
+	if($TeamItemCount[GameBase::getTeam(%player) @ %item] < $TeamItemMax[%item]) {
+		if (GameBase::getLOSInfo(%player,3)) {
+			// GetLOSInfo sets the following globals:
+			// 	los::position
+			// 	los::normal
+			// 	los::object
+			%obj = getObjectType($los::object);
+			if (%obj == "SimTerrain" || %obj == "InteriorShape") {
+				// Try to stick it straight up or down, otherwise
+				// just use the surface normal
+				%prot = GameBase::getRotation(%player);
+				%zRot = getWord(%prot,2);
+				if (Vector::dot($los::normal,"0 0 1") > 0.6) {
+					%rot = "0 0 " @ %zRot;
+				}
+				else {
+					if (Vector::dot($los::normal,"0 0 -1") > 0.6) {
+						%rot = "3.14159 0 " @ %zRot;
+					}
+					else {
+						%rot = Vector::getRotation($los::normal);
+					}
+				}
+				if(checkDeployArea(%client,$los::position)) {
+					%mSensor = newObject("","Sensor",DeployableMotionSensor,true);
+	   	      addToSet("MissionCleanup", %mSensor);
+					GameBase::setTeam(%mSensor,GameBase::getTeam(%player));
+					GameBase::setRotation(%mSensor,%rot);
+					GameBase::setPosition(%mSensor,$los::position);
+					Gamebase::setMapName(%mSensor,"Motion Sensor");
+					Client::sendMessage(%client,0,"Motion Sensor deployed");
+					playSound(SoundPickupBackpack,$los::position);
+					echo("MSG: ",%client," deployed a Motion Sensor");
+					return true;
+				}
+			}
+			else {
+				Client::sendMessage(%client,0,"Can only deploy on terrain or buildings");
+			}
+		}
+		else {
+			Client::sendMessage(%client,0,"Deploy position out of range");		
+		}
+	}
+	else																						  
+	 	Client::sendMessage(%client,0,"Deployable Item limit reached for " @ %item.description @ "s");
+	
+	return false;
+}
+
+//----------------------------------------------------------------------------
+
+function AmmoPack::onDrop(%player, %item)
+{
+	if($matchStarted) {
+		%item = Item::onDrop(%player,%item);
+		for(%i = 0; %i < 7 ; %i = %i +1) {
+			%numPack = 0;
+			%ammoItem = $AmmoPackItems[%i];
+			%maxnum = $ItemMax[Player::getArmor(%player), %ammoItem];
+			%pCount = Player::getItemCount(%player, %ammoItem);
+			if(%pCount > %maxnum) {
+				%numPack = %pCount - %maxnum;
+				Player::decItemCount(%player,%ammoItem,%numPack);
+			}	
+			if(%i == 0) {
+	 	    	%item.BulletAmmo = %numPack;
+			}
+			else if(%i == 1) {
+	 	    	%item.PlasmaAmmo = %numPack;
+			}
+			else if(%i == 2) {
+	 	    	%item.DiscAmmo = %numPack;
+			}
+			else if(%i == 3) {
+	 	    	%item.GrenadeAmmo = %numPack;
+			}
+			else if(%i == 4) {
+	 	    	%item.Grenade = %numPack;
+			}
+			else if(%i == 5) {
+	 	    	%item.MortarAmmo = %numPack;
+			}
+			else {
+	 	    	%item.MineAmmo = %numPack;
+			}
+		}
+	}
+}
+
+function AmmoPack::onCollision(%this,%object)
+{
+	if (getObjectType(%object) == "Player") {
+		%item = Item::getItemData(%this);
+		%count = Player::getItemCount(%object,%item);
+		if (Item::giveItem(%object,%item,Item::getCount(%this))) {
+			Item::playPickupSound(%this);
+			checkPacksAmmo(%object, %this);
+			Item::respawn(%this);
+		}
+	}
+}
+
+function checkPacksAmmo(%player, %item)
+{
+	for(%i = 0; %i < 7 ; %i = %i +1) {
+		%ammoItem = $AmmoPackItems[%i];
+		if(%i == 0) {
+	        %numAdd = %item.BulletAmmo;
+		}
+		else if(%i == 1) {
+	    	%numAdd = %item.PlasmaAmmo;
+		}
+		else if(%i == 2) {
+	    	%numAdd = %item.DiscAmmo;
+		}
+		else if(%i == 3) {
+	    	%numAdd = %item.GrenadeAmmo;
+		}
+		else if(%i == 4) {
+	    	%numAdd = %item.Grenade;
+		}
+		else if(%i == 5) {
+ 	    	%numAdd = %item.MortarAmmo;
+		}
+		else {
+			%numAdd = %item.MineAmmo;
+		}
+		Player::incItemCount(%player,%ammoItem,%numAdd);
+	}						 
+}
+
+function fillAmmoPack(%client)
+{
+	%player = Client::getOwnedObject(%client);
+	for(%i = 0; %i < 7 ; %i = %i +1) {
+		%item = $AmmoPackItems[%i];
+		%maxnum = $AmmoPackMax[%item];
+		%maxnum = checkResources(%player,%item,%maxnum); 
+		if(%maxnum) {
+			Player::incItemCount(%client,%item,%maxnum);
+			teamEnergyBuySell(%player,%item.price * %maxnum * -1);
+		}	
+	}
+}
+
+//----------------------------------------------------------------------------
+
+function PulseSensorPack::onUse(%player,%item)
+{
+	if (Player::getMountedItem(%player,$BackpackSlot) != %item) {
+		Player::mountItem(%player,%item,$BackpackSlot);
+	}
+	else {
+		Player::deployItem(%player,%item);
+	}
+}
+
+function PulseSensorPack::onDeploy(%player,%item,%pos)
+{
+	if (Item::deployShape(%player,"Pulse Sensor",DeployablePulseSensor,%item)) {
+		Player::decItemCount(%player,%item);
+		$TeamItemCount[GameBase::getTeam(%player) @ "PulseSensorPack"]++;
+	}
+}
+
+//----------------------------------------------------------------------------
+
+function DeployableSensorJammerPack::onUse(%player,%item)
+{
+	if (Player::getMountedItem(%player,$BackpackSlot) != %item) {
+		Player::mountItem(%player,%item,$BackpackSlot);
+	}
+	else {
+		Player::deployItem(%player,%item);
+	}
+}
+
+function DeployableSensorJammerPack::onDeploy(%player,%item,%pos)
+{
+	if (Item::deployShape(%player,"Sensor Jammer",DeployableSensorJammer,%item)) {
+		Player::decItemCount(%player,%item);
+		$TeamItemCount[GameBase::getTeam(%player) @ "DeployableSensorJammerPack"]++;
+	}
+}
+
+//----------------------------------------------------------------------------
 
 function CameraPack::onUse(%player,%item)
 {
@@ -99,6 +510,23 @@ function CameraPack::deployShape(%player,%item)
 
 //----------------------------------------------------------------------------
 
+function TurretPack::onUse(%player,%item)
+{
+	if (Player::getMountedItem(%player,$BackpackSlot) != %item) {
+		Player::mountItem(%player,%item,$BackpackSlot);
+	}
+	else {
+		Player::deployItem(%player,%item);
+	}
+}
+
+function TurretPack::onDeploy(%player,%item,%pos)
+{
+	if (TurretPack::deployShape(%player,%item)) {
+		Player::decItemCount(%player,%item);
+	}
+}
+
 function CountObjects(%set,%name,%num) 
 {
 	%count = 0;
@@ -108,6 +536,63 @@ function CountObjects(%set,%name,%num)
 			%count++;
 	}
 	return %count;
+}
+
+function TurretPack::deployShape(%player,%item)
+{
+	%client = Player::getClient(%player);
+	if($TeamItemCount[GameBase::getTeam(%player) @ %item] < $TeamItemMax[%item]) {
+		if (GameBase::getLOSInfo(%player,3)) {
+			%obj = getObjectType($los::object);
+			if (%obj == "SimTerrain" || %obj == "InteriorShape") {
+	    		%set = newObject("set",SimSet);
+				%num = containerBoxFillSet(%set,$StaticObjectType,$los::position,$TurretBoxMaxLength,$TurretBoxMaxWidth,$TurretBoxMaxHeight,0);
+				%num = CountObjects(%set,"DeployableTurret",%num);
+				deleteObject(%set);
+				if($MaxNumTurretsInBox > %num) {
+		    		%set = newObject("set",SimSet);
+					%num = containerBoxFillSet(%set,$StaticObjectType,$los::position,$TurretBoxMinLength,$TurretBoxMinWidth,$TurretBoxMinHeight,0);
+					%num = CountObjects(%set,"DeployableTurret",%num);
+					deleteObject(%set);
+					if(0 == %num) {
+						if (Vector::dot($los::normal,"0 0 1") > 0.7) {
+							if(checkDeployArea(%client,$los::position)) {
+								%rot = GameBase::getRotation(%player); 
+								%turret = newObject("remoteTurret","Turret",DeployableTurret,true);
+	                     addToSet("MissionCleanup", %turret);
+								GameBase::setTeam(%turret,GameBase::getTeam(%player));
+								GameBase::setPosition(%turret,$los::position);
+								GameBase::setRotation(%turret,%rot);
+								Gamebase::setMapName(%turret,"RMT Turret#" @ $totalNumTurrets++ @ " " @ Client::getName(%client));
+								Client::sendMessage(%client,0,"Remote Turret deployed");
+								playSound(SoundPickupBackpack,$los::position);
+								$TeamItemCount[GameBase::getTeam(%player) @ "TurretPack"]++;
+								echo("MSG: ",%client," deployed a Remote Turret");
+								//	Remote turrets - kill points to player that deploy them
+								// Client::setOwnedObject(%client, %turret); 
+								// Client::setOwnedObject(%client, %player);
+								return true;
+							}
+						}
+						else 
+							Client::sendMessage(%client,0,"Can only deploy on flat surfaces");
+					} 
+					else
+						Client::sendMessage(%client,0,"Frequency Overload - Too close to other remote turrets");
+				}
+			   else 
+					Client::sendMessage(%client,0,"Interference from other remote turrets in the area");
+			}
+			else 
+				Client::sendMessage(%client,0,"Can only deploy on terrain or buildings");
+		}
+		else 
+			Client::sendMessage(%client,0,"Deploy position out of range");
+	}
+	else																						  
+	 	Client::sendMessage(%client,0,"Deployable Item limit reached for " @ %item.description @ "s");
+
+	return false;
 }
 
 function checkDeployArea(%client,%pos)
@@ -133,6 +618,8 @@ function checkDeployArea(%client,%pos)
 		
 
 }
+
+//----------------------------------------------------------------------------
 
 function Item::deployShape(%player,%name,%shape,%item)
 {
